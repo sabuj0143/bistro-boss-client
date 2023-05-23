4772c3fde48cc99752061ad4d82db89e40d1a9a0
sabuj0143 <sabujhowlader011@gmai.com>
2023-05-23 10:30:24 +0600

    Navbar added

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../pages/Sherad/Footer/Footer";
+import NavBar from "../pages/Sherad/NavBar/NavBar";
 
 const Main = () => {
     return (
         <>
+            <NavBar></NavBar>
             <Outlet></Outlet>
             <Footer></Footer>
         </>
