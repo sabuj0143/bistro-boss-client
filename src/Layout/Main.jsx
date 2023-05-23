2854a6ddb0697e89453d1644823f5e0eda47e8e1
sabuj0143 <sabujhowlader011@gmai.com>
2023-05-23 10:04:26 +0600

    Footer added

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../pages/Sherad/Footer/Footer";
 
 const Main = () => {
     return (
         <>
             <Outlet></Outlet>
+            <Footer></Footer>
         </>
     );
 };
