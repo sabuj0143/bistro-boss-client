ac88e96b3cca25b990cd4d82c71e164ac893d1aa
sabuj0143 <sabujhowlader011@gmai.com>
2023-05-25 00:46:50 +0600

    Parallax more added

--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -1,4 +1,7 @@
 import { Helmet } from 'react-helmet-async';
+import Cover from '../../Sherad/Cover/Cover';
+import menuImg from '../../../assets/menu/banner3.jpg'
+import PopularMenu from '../../Home/PopularMenu/PopularMenu';
 
 const Menu = () => {
     return (
@@ -6,7 +9,12 @@ const Menu = () => {
             <Helmet>
                 <title>Bistro Boss | Menu</title>
             </Helmet>
-            <h3>Menu menu menu</h3>
+            <Cover img={menuImg} title={"Our Menu"}></Cover>
+            <PopularMenu></PopularMenu>
+            <Cover img={menuImg} title={"Our Menu"}></Cover>
+            <PopularMenu></PopularMenu>
+            <Cover img={menuImg} title={"Our Menu"}></Cover>
+            <PopularMenu></PopularMenu>
         </div>
     );
 };
