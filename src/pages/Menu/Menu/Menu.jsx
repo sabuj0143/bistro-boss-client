905fdb818d390845a94b9d4971260700d8458e99
sabuj0143 <sabujhowlader011@gmai.com>
2023-05-24 21:12:55 +0600

    Page title added

---
+++ src/pages/Menu/Menu/Menu.jsx
@@ -0,0 +1,14 @@
+import { Helmet } from 'react-helmet-async';
+
+const Menu = () => {
+    return (
+        <div>
+            <Helmet>
+                <title>Bistro Boss | Menu</title>
+            </Helmet>
+            <h3>Menu menu menu</h3>
+        </div>
+    );
+};
+
+export default Menu;
