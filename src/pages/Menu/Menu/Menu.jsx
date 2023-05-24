2bae204ea81ebc53e719a8693996aba6038ead4f
sabuj0143 <sabujhowlader011@gmai.com>
2023-05-25 01:05:47 +0600

    Custom Hook added

--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -1,7 +1,6 @@
 import { Helmet } from 'react-helmet-async';
 import Cover from '../../Sherad/Cover/Cover';
 import menuImg from '../../../assets/menu/banner3.jpg'
-import PopularMenu from '../../Home/PopularMenu/PopularMenu';
 
 const Menu = () => {
     return (
@@ -10,11 +9,6 @@ const Menu = () => {
                 <title>Bistro Boss | Menu</title>
             </Helmet>
             <Cover img={menuImg} title={"Our Menu"}></Cover>
-            <PopularMenu></PopularMenu>
-            <Cover img={menuImg} title={"Our Menu"}></Cover>
-            <PopularMenu></PopularMenu>
-            <Cover img={menuImg} title={"Our Menu"}></Cover>
-            <PopularMenu></PopularMenu>
         </div>
     );
 };
