8728cf68b02a6071dfc2c4f3992cb72efb8e1bdd
sabuj0143 <sabujhowlader011@gmai.com>
2023-05-26 20:44:52 +0600

    Navbar and footer route login page hiden

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,13 +1,17 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Footer from "../pages/Sherad/Footer/Footer";
 import NavBar from "../pages/Sherad/NavBar/NavBar";
 
 const Main = () => {
+
+    const location = useLocation();
+    const noHeaderFOoter = location.pathname.includes("login");
+
     return (
         <>
-            <NavBar></NavBar>
+            {noHeaderFOoter || <NavBar></NavBar>}
             <Outlet></Outlet>
-            <Footer></Footer>
+            {noHeaderFOoter || <Footer></Footer>}
         </>
     );
 };
